0ca189861945d5adbe5d9f23ba71741795e4954b
ilaza <ilaza@softserveinc.com>
2020-01-21 15:36:35 +0200

    TextFixtureClass and TestFixtureDataClass created

---
+++ TestCaseClass.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using NUnit.ListCollectionTests.TestData;
+using System.Collections.Generic;
+
+namespace NUnit.ListCollectionTests
+{
+    public class TestCaseClass
+    {
+        [Test]
+        [TestCaseSource(typeof(TestCaseDataClass), "TestCases")]
+        public void RemoveAllTest(List<int> testCollection, int predict)
+        {
+            var testCollectionClone = new List<int>();
+            testCollectionClone.AddRange(testCollection);
+
+            for (int i = testCollection.Count - 1; i >= 0; i--)
+            {
+                if (testCollection[i] > predict)
+                {
+                    testCollection.Remove(testCollection[i]);
+                }
+            }
+            testCollectionClone.RemoveAll(x => x > predict);
+
+            Assert.That(testCollectionClone, Is.EquivalentTo(testCollection));
+        }
+    }
+}
--- TestData/TestCaseDataClass.cs
+++ TestData/TestCaseDataClass.cs
@@ -8,6 +8,18 @@ namespace NUnit.ListCollectionTests.TestData
 {
     public class TestCaseDataClass
     {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, 5);
+                yield return new TestCaseData(new List<int>() { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 }, 5);
+                yield return new TestCaseData(new List<int>() { 11, 22, 33, 44, 55, 66, 77, 88, 99, 00 }, 5);
+                yield return new TestCaseData(new List<int>() { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 }, 5);
+                yield return new TestCaseData(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, 5);
+            }
+        }
 
+        public static int predictForRemoveAll = 5;
     }
 }
